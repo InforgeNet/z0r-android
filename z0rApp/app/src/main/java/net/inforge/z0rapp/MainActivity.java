7d041788fb1099fcda7dc8fb6e3d2a97cd342363
Federico Mastrini <federicomastrini93@gmail.com>
2015-05-16 20:04:37 +0200

    Update

--- z0rApp/app/src/main/java/net/inforge/z0rapp/MainActivity.java
+++ z0rApp/app/src/main/java/net/inforge/z0rapp/MainActivity.java
@@ -1,5 +1,10 @@
 package net.inforge.z0rapp;
 
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,6 +14,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import java.io.*;
+import java.net.*;
+
+import javax.net.ssl.HttpsURLConnection;
+
 
 
 public class MainActivity extends ActionBarActivity {
@@ -29,37 +51,135 @@ public class MainActivity extends ActionBarActivity {
         shrink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // viene preso il valore nel campo testo facoltativo
-                Editable link = customLink.getText();
-                // viene fatto il controllo dell'url in clipboard
-                if (check_clipboard() != "errore") {
-                    // se il link va bene si prosegue all shirnking
-                    if (link.length()<1) {
-                        // se il campo custom è vuoto non c'è personalizzazione
-                        shrinking();
+                show_toast(get_clipboard()); // test del contenuto della clipboard
+                /*
+                String link_toshrink = check_clipboard(get_clipboard()); // Controlla la clipboard e salva il contenuto
+                if (link_toshrink != "errore") { // Se il contenuto va bene, si prosegue
+                    Editable custom_link = customLink.getText(); // Prendo il valore nel campo testo facoltativo
+                    if (custom_link.length() < 1) { // se il link va bene si prosegue allo shrinking
+                        shrinking(link_toshrink); // se il campo custom è vuoto non c'è personalizzazione
                     } else {
-                        // altrimenti si procede con lo shrink personalizzato
-                        custom_shrinking(link);
+                        custom_shrinking(link_toshrink, custom_link); // altrimenti si procede con lo shrink personalizzato
                     }
-                } else {
-                    // mostra errore clipboard
+                } else { // Se il contenuto da errore (non c'è contenuto o non è un URL valido) mostra un avviso
+                    show_toast("La clipboard non contiene un URL valido");
                 }
+                */
+
             }
         });
     }
 
-    public void shrinking(){
+    // Funzione che mostra un toast
+    public void show_toast(String message) {
+        Context context = getApplicationContext();
+        CharSequence text = message;
+        int duration = Toast.LENGTH_SHORT;
 
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
-    public void custom_shrinking(Editable link){
-
+    // Mi creo una funzione per prendere il contenuto della clipboard per comodità
+    public String get_clipboard(){
+        if (is_clipboard()) {
+            return ((ClipboardManager)getSystemService(this.CLIPBOARD_SERVICE)).getPrimaryClip().toString();
+        } else {
+            return "";
+        }
     }
 
-    public java.lang.String check_clipboard(){
-        return "ciao";
+    // Mi credo una funzione per impostare il contenuto della clipboard per comodità
+    public void set_clipboard(String link) {
+        ClipData clip = ClipData.newPlainText("label",link);
+        ((ClipboardManager)getSystemService(this.CLIPBOARD_SERVICE)).setPrimaryClip(clip);
     }
 
+    // Funzione che dice se la clipboard ha contenuto o no
+    public boolean is_clipboard(){
+        return ((ClipboardManager)getSystemService(this.CLIPBOARD_SERVICE)).hasPrimaryClip();
+    }
+
+    // Funzione che fa la richiesta GET a z0r.it
+    public void shrinking(java.lang.CharSequence link){
+        StringBuilder sb = new StringBuilder();
+        URLConnection urlConn = null;
+        InputStreamReader in = null;
+        try {
+            URL url = new URL("http://z0r.it/yourls-api.php?signature=4e4b657a91&action=shorturl&format=simply&url=" + link + "&title=upload_wth_z0r_andr0id");
+            urlConn = url.openConnection();
+            if (urlConn != null)
+                urlConn.setReadTimeout(60 * 1000);
+            if (urlConn != null && urlConn.getInputStream() != null) {
+                in = new InputStreamReader(urlConn.getInputStream(),
+                        Charset.defaultCharset());
+                BufferedReader bufferedReader = new BufferedReader(in);
+                if (bufferedReader != null) {
+                    int cp;
+                    while ((cp = bufferedReader.read()) != -1) {
+                        sb.append((char) cp);
+                    }
+                    bufferedReader.close();
+                }
+            }
+            in.close();
+        } catch (Exception e) {
+
+        }
+        set_clipboard(sb.toString());
+    }
+
+    // Funzione che fa la richiesta GET a z0r.it con personalizzazione
+    public void custom_shrinking(java.lang.CharSequence link, Editable custom_link){
+        StringBuilder sb = new StringBuilder();
+        URLConnection urlConn = null;
+        InputStreamReader in = null;
+        try {
+            URL url = new URL("http://z0r.it/yourls-api.php?signature=4e4b657a91&action=shorturl&keyword=" + custom_link + "&format=simply&url=" + link + "&title=upload_wth_z0r_andr0id");
+            urlConn = url.openConnection();
+            if (urlConn != null)
+                urlConn.setReadTimeout(60 * 1000);
+            if (urlConn != null && urlConn.getInputStream() != null) {
+                in = new InputStreamReader(urlConn.getInputStream(),
+                        Charset.defaultCharset());
+                BufferedReader bufferedReader = new BufferedReader(in);
+                if (bufferedReader != null) {
+                    int cp;
+                    while ((cp = bufferedReader.read()) != -1) {
+                        sb.append((char) cp);
+                    }
+                    bufferedReader.close();
+                }
+            }
+            in.close();
+        } catch (Exception e) {
+
+        }
+        set_clipboard(sb.toString());
+    }
+
+    // Controlla il contenuto della clipboard
+    public String check_clipboard(String cb){
+        if (is_clipboard()) { // Se la clipboard è vuota restituisce "errore"
+            Pattern url_regex = Pattern.compile("(https?|ftp|file)://[-A-Za-z0-9\\+&@#/%?=~_|!:,.;]*\\.[-A-Za-z0-9\\+&@#/%=~_|():?]+");
+            Matcher match = url_regex.matcher(cb);
+            if (match.matches()) {
+                return cb;
+            } else {
+                cb = "http://"+cb;
+                match = url_regex.matcher(cb);
+                if (match.matches()) {
+                    return cb;
+                } else {
+                    return "errore";
+                }
+            }
+        } else {
+            return "errore";
+        }
+    }
+
+    /* Crea un menu, per ora non serve
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -81,4 +201,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
 }
